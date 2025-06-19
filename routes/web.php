b8ad7b54b1cf7c4639c19f53c4c456c10ee7b22c
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-06-19 08:03:51 +0300

    praktikum web 2

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/produk', [App\Http\Controllers\HomeController::class, 'produk'])->name('products.index');
+
+// Admin Routes
+Route::middleware(['isAdmin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users');
+    // tambahkan route admin lainnya di sini
+});
+
+// User Routes
+Route::middleware(['auth'])->group(function () {
+    // tambahkan route user lainnya di sini
+});
